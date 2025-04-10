c4a3e917aba5e074a93cd42abb3a140c0d003d8f
Lidia Yamamura <108353487+RinoaYK@users.noreply.github.com>
2025-04-10 10:14:31 -0300

    Changed to render
    Mudando o backend para o render.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-	 apiUrl: 'https://barber-shop-api-production.up.railway.app/'
-	 // apiUrl: 'https://barber-shop-api-wojh.onrender.com/'
+	 // apiUrl: 'https://barber-shop-api-production.up.railway.app/'
+	 apiUrl: 'https://barber-shop-api-wojh.onrender.com/'
 }
